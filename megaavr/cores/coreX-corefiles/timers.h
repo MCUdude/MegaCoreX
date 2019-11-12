5d8df2368e5d015127710261bc61c0414e1ea6f6
egil <egil@kvaleberg.no>
2019-11-12 13:53:59 +0100

    Fix period of PWM timers so that the value 0 is to LOW, the value 255 is HIGH, and any value inbetween is a corresponding PWM pulse. The prevous behaviour was that the range was 0 to 256, which is not in accordance to the Arduino standard.

--- megaavr/cores/coreX-corefiles/timers.h
+++ megaavr/cores/coreX-corefiles/timers.h
@@ -5,7 +5,7 @@
 #define TIME_TRACKING_TIMER_DIVIDER   1     // Timer F_CPU Clock divider (can be 1 or 2)
 #define TIME_TRACKING_TIMER_COUNT     (F_CPU/(1000*TIME_TRACKING_TIMER_DIVIDER)) // Should correspond to exactly 1 ms, i.e. millis()
 
-#define PWM_TIMER_PERIOD   0xFF  // For frequency
+#define PWM_TIMER_PERIOD   0xFE  // For frequency
 #define PWM_TIMER_COMPARE  0x80  // For duty cycle
 
 #endif
