a16ef7cab5aae4a8afd84d87a7c246aafe4636c4
MCUdude <hansibull@gmail.com>
2020-07-26 11:08:35 +0200

    Fix and improve Nano Every pinout Fix an issue where USART0 were incorrectly routed. Closes #80. Swap Serial2 (hardware USART0) so the default RX and TX pins are routed to pins that are broken put

--- megaavr/variants/nano-every/pins_arduino.h
+++ megaavr/variants/nano-every/pins_arduino.h
@@ -28,133 +28,9 @@
 
 #define NANO_EVERY_PINOUT
 #define NONA4809_PINOUT
+#define __AVR_ATmegax09__
 
-#define NUM_DIGITAL_PINS               41
-#define NUM_ANALOG_INPUTS              16
-#define NUM_RESERVED_PINS              0
-#define NUM_INTERNALLY_USED_PINS       0
-#define NUM_I2C_PINS                   2  // (SDA / SCL)
-#define NUM_SPI_PINS                   3  // (MISO / MOSI / SCK)
-#define NUM_TOTAL_FREE_PINS            (NUM_DIGITAL_PINS)
-#define NUM_TOTAL_PINS                 (NUM_DIGITAL_PINS + NUM_RESERVED_PINS + NUM_INTERNALLY_USED_PINS + NUM_I2C_PINS + NUM_SPI_PINS)
-#define ANALOG_INPUT_OFFSET            14
-#define digitalPinToAnalogInput(p)     (((p) == A0) ? (3) : ((p) == A1) ? (2) : ((p) == A2) ? (1): ((p) == A3) ? (0): ((p) == A4) ? (12): ((p) == A5) ? (13): ((p) == A6) ? (4): ((p) == A7) ? (5): ((p) == A8) ? (7): ((p) == A9) ? (10) : ((p) == A10) ? (9): ((p) == A11) ? (8): ((p) == A12) ? (11): ((p) == A13) ? (14): ((p) == A14) ? (15) :((p) == A15) ? (29) : NOT_A_PIN)
-#define digitalOrAnalogPinToDigital(p) (((p) <= 40) ? (p) : NOT_A_PIN)
-
-#define MILLIS_USE_TIMERB3 // Use TCB3 for millis generation
-
-#define EXTERNAL_NUM_INTERRUPTS     (47)
-
-#define digitalPinHasPWM(p)         ((p) == 3 || (p) == 5 || (p) == 6 || (p) == 9 || (p) == 10 || (p) == 27)
-
-// Timer pin mapping
-#define TCA0_PINS PORTMUX_TCA0_PORTB_gc
-#define TCB0_PINS PORTMUX_TCB0_bm
-#define TCB1_PINS PORTMUX_TCB1_bm
-#define TCB2_PINS 0x00
-#define TCB3_PINS 0x00
-
-// SPI 0
-// Pinswap enabled by default and no alternative available
-#define SPI_INTERFACES_COUNT 1
-#define SPI_MUX              (PORTMUX_SPI0_ALT2_gc)
-#define PIN_SPI_MISO         (12)
-#define PIN_SPI_SCK          (13)
-#define PIN_SPI_MOSI         (11)
-#define PIN_SPI_SS           (8)
-static const uint8_t SS   = PIN_SPI_SS;
-static const uint8_t MOSI = PIN_SPI_MOSI;
-static const uint8_t MISO = PIN_SPI_MISO;
-static const uint8_t SCK  = PIN_SPI_SCK;
-
-// TWI 0
-// No pinswap enabled by default, and no alternative available
-#define TWI_MUX              (PORTMUX_TWI0_DEFAULT_gc)
-#define PIN_WIRE_SDA         (22)
-#define PIN_WIRE_SCL         (23)
-static const uint8_t SDA = PIN_WIRE_SDA;
-static const uint8_t SCL = PIN_WIRE_SCL;
-
-// Nano Every debug USART (not available on headers, only via the SAMD11 virtual COM port)
-// USART3 on mega4809 (alternative pins)
-// Mapped to HWSERIAL0 in Serial library
-#define HWSERIAL0                       (&USART3)
-#define HWSERIAL0_DRE_VECTOR            (USART3_DRE_vect)
-#define HWSERIAL0_DRE_VECTOR_NUM        (USART3_DRE_vect_num)
-#define HWSERIAL0_RXC_VECTOR            (USART3_RXC_vect)
-#define HWSERIAL0_MUX                   (PORTMUX_USART3_ALT1_gc)
-#define HWSERIAL0_MUX_PINSWAP_1         (PORTMUX_USART3_DEFAULT_gc)
-#define PIN_WIRE_HWSERIAL0_TX           (25)
-#define PIN_WIRE_HWSERIAL0_RX           (24)
-#define PIN_WIRE_HWSERIAL0_TX_PINSWAP_1 (9)
-#define PIN_WIRE_HWSERIAL0_RX_PINSWAP_1 (10)
-
-// Nano every main USART available on Arduino header pins
-// USART1 on mega4809 (alternative pins)
-// Mapped to HWSERIAL1 in Serial library
-#define HWSERIAL1                       (&USART1)
-#define HWSERIAL1_DRE_VECTOR            (USART1_DRE_vect)
-#define HWSERIAL1_DRE_VECTOR_NUM        (USART1_DRE_vect_num)
-#define HWSERIAL1_RXC_VECTOR            (USART1_RXC_vect)
-#define HWSERIAL1_MUX                   (PORTMUX_USART1_ALT1_gc)
-#define HWSERIAL1_MUX_PINSWAP_1         (PORTMUX_USART1_DEFAULT_gc)
-#define PIN_WIRE_HWSERIAL1_TX           (1)
-#define PIN_WIRE_HWSERIAL1_RX           (0)
-#define PIN_WIRE_HWSERIAL1_TX_PINSWAP_1 (34)
-#define PIN_WIRE_HWSERIAL1_RX_PINSWAP_1 (35)
-
-// Nano Every pare USART available on testpoints
-// USART0 on mega4809 (alternative pins)
-// Mapped to HWSERIAL2 in Serial library
-#define HWSERIAL2                       (&USART0)
-#define HWSERIAL2_DRE_VECTOR            (USART0_DRE_vect)
-#define HWSERIAL2_DRE_VECTOR_NUM        (USART0_DRE_vect_num)
-#define HWSERIAL2_RXC_VECTOR            (USART0_RXC_vect)
-#define HWSERIAL2_MUX                   (PORTMUX_USART0_ALT1_gc)
-#define HWSERIAL2_MUX_PINSWAP_1         (PORTMUX_USART0_DEFAULT_gc)
-#define PIN_WIRE_HWSERIAL2_TX           (24)
-#define PIN_WIRE_HWSERIAL2_RX           (23)
-#define PIN_WIRE_HWSERIAL2_TX_PINSWAP_1 (2)
-#define PIN_WIRE_HWSERIAL2_RX_PINSWAP_1 (7)
-
-#define HWSERIAL3_MUX         (PORTMUX_USART2_NONE_gc)
-
-#define LED_BUILTIN 13
-
-#define PIN_A0   (14) // AIN3
-#define PIN_A1   (15) // AIN2
-#define PIN_A2   (16) // AIN1
-#define PIN_A3   (17) // AIN0
-#define PIN_A4   (18) // AIN12
-#define PIN_A5   (19) // AIN13
-#define PIN_A6   (20) // AIN4
-#define PIN_A7   (21) // AIN5
-#define PIN_A8   (39) // AIN7
-#define PIN_A9   (13) // AIN10
-#define PIN_A10  (12) // AIN9
-#define PIN_A11  (11) // AIN8
-#define PIN_A12  (8)  // AIN11
-#define PIN_A13  (6)  // AIN14
-#define PIN_A14  (3)  // AIN15
-#define PIN_A15  (29) // AIN6
-
-static const uint8_t A0  = PIN_A0;
-static const uint8_t A1  = PIN_A1;
-static const uint8_t A2  = PIN_A2;
-static const uint8_t A3  = PIN_A3;
-static const uint8_t A4  = PIN_A4;
-static const uint8_t A5  = PIN_A5;
-static const uint8_t A6  = PIN_A6;
-static const uint8_t A7  = PIN_A7;
-static const uint8_t A8  = PIN_A8;
-static const uint8_t A9  = PIN_A9;
-static const uint8_t A10 = PIN_A10;
-static const uint8_t A11 = PIN_A11;
-static const uint8_t A12 = PIN_A12;
-static const uint8_t A13 = PIN_A13;
-static const uint8_t A14 = PIN_A14;
-static const uint8_t A15 = PIN_A15;
-
+// Arduino pin macros
 #define PIN_PC5 0
 #define PIN_PC4 1
 #define PIN_PA0 2
@@ -197,7 +73,133 @@ static const uint8_t A15 = PIN_A15;
 #define PIN_PD7 39
 #define PIN_PF6 40
 
-#define PINS_COUNT    (41u)
+#define PINS_COUNT                     41
+#define NUM_DIGITAL_PINS               PINS_COUNT
+#define NUM_ANALOG_INPUTS              16
+#define NUM_RESERVED_PINS              0
+#define NUM_INTERNALLY_USED_PINS       0
+#define NUM_I2C_PINS                   2  // (SDA / SCL)
+#define NUM_SPI_PINS                   3  // (MISO / MOSI / SCK)
+#define NUM_TOTAL_FREE_PINS            (NUM_DIGITAL_PINS)
+#define NUM_TOTAL_PINS                 (NUM_DIGITAL_PINS + NUM_RESERVED_PINS + NUM_INTERNALLY_USED_PINS + NUM_I2C_PINS + NUM_SPI_PINS)
+#define ANALOG_INPUT_OFFSET            14
+#define LED_BUILTIN                    13
+#define EXTERNAL_NUM_INTERRUPTS        47
+#define digitalPinToAnalogInput(p)     (((p) == A0) ? (3) : ((p) == A1) ? (2) : ((p) == A2) ? (1): ((p) == A3) ? (0): ((p) == A4) ? (12): ((p) == A5) ? (13): ((p) == A6) ? (4): ((p) == A7) ? (5): ((p) == A8) ? (7): ((p) == A9) ? (10) : ((p) == A10) ? (9): ((p) == A11) ? (8): ((p) == A12) ? (11): ((p) == A13) ? (14): ((p) == A14) ? (15) :((p) == A15) ? (29) : NOT_A_PIN)
+#define digitalOrAnalogPinToDigital(p) (((p) <= 40) ? (p) : NOT_A_PIN)
+
+// Timer to use for millis/micros
+#define MILLIS_USE_TIMERB3
+
+// PWM pins
+#define digitalPinHasPWM(p)            ((p) == 3 || (p) == 5 || (p) == 6 || (p) == 9 || (p) == 10 || (p) == 27)
+
+// Timer pin mapping
+#define TCA0_PINS PORTMUX_TCA0_PORTB_gc
+#define TCB0_PINS PORTMUX_TCB0_bm
+#define TCB1_PINS PORTMUX_TCB1_bm
+#define TCB2_PINS 0x00
+#define TCB3_PINS 0x00
+
+// SPI 0
+// Pinswap enabled by default and no alternative available
+#define SPI_INTERFACES_COUNT 1
+#define SPI_MUX              (PORTMUX_SPI0_ALT2_gc)
+#define PIN_SPI_MISO         (PIN_PE1)
+#define PIN_SPI_SCK          (PIN_PE2)
+#define PIN_SPI_MOSI         (PIN_PE0)
+#define PIN_SPI_SS           (PIN_PE3)
+static const uint8_t SS   = PIN_SPI_SS;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
+
+// TWI 0
+// No pinswap enabled by default, and no alternative available
+#define TWI_MUX              (PORTMUX_TWI0_DEFAULT_gc)
+#define PIN_WIRE_SDA         (PIN_PA2)
+#define PIN_WIRE_SCL         (PIN_PA3)
+static const uint8_t SDA = PIN_WIRE_SDA;
+static const uint8_t SCL = PIN_WIRE_SCL;
+
+// Nano Every debug USART (not available on headers, only via the SAMD11 virtual COM port)
+// USART3 on mega4809 (alternative pins)
+// Mapped to HWSERIAL0 in Serial library
+#define HWSERIAL0                       (&USART3)
+#define HWSERIAL0_DRE_VECTOR            (USART3_DRE_vect)
+#define HWSERIAL0_DRE_VECTOR_NUM        (USART3_DRE_vect_num)
+#define HWSERIAL0_RXC_VECTOR            (USART3_RXC_vect)
+#define HWSERIAL0_MUX                   (PORTMUX_USART3_ALT1_gc)
+#define HWSERIAL0_MUX_PINSWAP_1         (PORTMUX_USART3_DEFAULT_gc)
+#define PIN_WIRE_HWSERIAL0_TX           (PIN_PB4)
+#define PIN_WIRE_HWSERIAL0_RX           (PIN_PB5)
+#define PIN_WIRE_HWSERIAL0_TX_PINSWAP_1 (PIN_PB0)
+#define PIN_WIRE_HWSERIAL0_RX_PINSWAP_1 (PIN_PB1)
+
+// Nano every main USART available on Arduino header pins
+// USART1 on mega4809 (alternative pins)
+// Mapped to HWSERIAL1 in Serial library
+#define HWSERIAL1                       (&USART1)
+#define HWSERIAL1_DRE_VECTOR            (USART1_DRE_vect)
+#define HWSERIAL1_DRE_VECTOR_NUM        (USART1_DRE_vect_num)
+#define HWSERIAL1_RXC_VECTOR            (USART1_RXC_vect)
+#define HWSERIAL1_MUX                   (PORTMUX_USART1_ALT1_gc)
+#define HWSERIAL1_MUX_PINSWAP_1         (PORTMUX_USART1_DEFAULT_gc)
+#define PIN_WIRE_HWSERIAL1_TX           (PIN_PC4)
+#define PIN_WIRE_HWSERIAL1_RX           (PIN_PC5)
+#define PIN_WIRE_HWSERIAL1_TX_PINSWAP_1 (PIN_PC0)
+#define PIN_WIRE_HWSERIAL1_RX_PINSWAP_1 (PIN_PC1)
+
+// Nano Every pare USART available on testpoints
+// USART0 on mega4809 (default pins)
+// Mapped to HWSERIAL2 in Serial library
+#define HWSERIAL2                       (&USART0)
+#define HWSERIAL2_DRE_VECTOR            (USART0_DRE_vect)
+#define HWSERIAL2_DRE_VECTOR_NUM        (USART0_DRE_vect_num)
+#define HWSERIAL2_RXC_VECTOR            (USART0_RXC_vect)
+#define HWSERIAL2_MUX                   (PORTMUX_USART0_DEFAULT_gc)
+#define HWSERIAL2_MUX_PINSWAP_1         (PORTMUX_USART0_ALT1_gc)
+#define PIN_WIRE_HWSERIAL2_TX           (PIN_PA0)
+#define PIN_WIRE_HWSERIAL2_RX           (PIN_PA1)
+#define PIN_WIRE_HWSERIAL2_TX_PINSWAP_1 (PIN_PA4)
+#define PIN_WIRE_HWSERIAL2_RX_PINSWAP_1 (PIN_PA5)
+
+// Hardware USART2 not in use because no pins are broken out
+#define HWSERIAL3_MUX         (PORTMUX_USART2_NONE_gc)
+
+// Analog pins
+#define PIN_A0   PIN_PD3 // AIN3
+#define PIN_A1   PIN_PD2 // AIN2
+#define PIN_A2   PIN_PD1 // AIN1
+#define PIN_A3   PIN_PD0 // AIN0
+#define PIN_A4   PIN_PF2 // AIN12
+#define PIN_A5   PIN_PF3 // AIN13
+#define PIN_A6   PIN_PD4 // AIN4
+#define PIN_A7   PIN_PD5 // AIN5
+#define PIN_A8   PIN_PD7 // AIN7
+#define PIN_A9   PIN_PE2 // AIN10
+#define PIN_A10  PIN_PE1 // AIN9
+#define PIN_A11  PIN_PE0 // AIN8
+#define PIN_A12  PIN_PE3 // AIN11
+#define PIN_A13  PIN_PF4 // AIN14
+#define PIN_A14  PIN_PF5 // AIN15
+#define PIN_A15  PIN_PD6 // AIN6
+static const uint8_t A0  = PIN_A0;
+static const uint8_t A1  = PIN_A1;
+static const uint8_t A2  = PIN_A2;
+static const uint8_t A3  = PIN_A3;
+static const uint8_t A4  = PIN_A4;
+static const uint8_t A5  = PIN_A5;
+static const uint8_t A6  = PIN_A6;
+static const uint8_t A7  = PIN_A7;
+static const uint8_t A8  = PIN_A8;
+static const uint8_t A9  = PIN_A9;
+static const uint8_t A10 = PIN_A10;
+static const uint8_t A11 = PIN_A11;
+static const uint8_t A12 = PIN_A12;
+static const uint8_t A13 = PIN_A13;
+static const uint8_t A14 = PIN_A14;
+static const uint8_t A15 = PIN_A15;
 
 #ifdef ARDUINO_MAIN
 
