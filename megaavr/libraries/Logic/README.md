4d3af09c12186aad63fb4e0b493f4f8492489232
MCUdude <hansibull@gmail.com>
2020-08-14 21:42:25 +0200

    Update Logic library Incorporate improvements @SpenceKonde has added Closes #83

--- megaavr/libraries/Logic/README.md
+++ megaavr/libraries/Logic/README.md
@@ -1,12 +1,14 @@
 # Logic
 A library for interfacing with the CCL (Configurable Custom Logic) peripherals of the megaAVR-0 MCUs.  
-Developed by MCUdude for use with [MegaCoreX](https://github.com/MCUdude/MegaCoreX).  
+Developed by MCUdude for use with [MegaCoreX](https://github.com/MCUdude/MegaCoreX), adapted to megaAVR ATtiny parts by [Tadashi G. Takaoka](https://github.com/tgtakaoka), and to DA-series by [Spence Konde](https://github.com/SpenceKonde)
 The megaAVR-0 has four independent internal logic blocks that can be individually customized.  
-More useful information about CCL can be found in the [Microchip Application Note TB3218](http://ww1.microchip.com/downloads/en/AppNotes/TB3218-Getting-Started-with-CCL-90003218A.pdf) and in the [megaAVR-0 family data sheet](http://ww1.microchip.com/downloads/en/DeviceDoc/megaAVR0-series-Family-Data-Sheet-DS40002015B.pdf).
+All of the megaTiny parts have 2 blocks of CCL available. The examples included assume the use of megaTinyCore in their defines to detect the applicable part.
+More useful information about CCL can be found in the [Microchip Application Note TB3218](http://ww1.microchip.com/downloads/en/AppNotes/TB3218-Getting-Started-with-CCL-90003218A.pdf) and in the [megaAVR-0 family data sheet](http://ww1.microchip.com/downloads/en/DeviceDoc/megaAVR0-series-Family-Data-Sheet-DS40002015B.pdf), part in question.
 
 
 ## Logic
-Class for interfacing with the built-in logic block. use the predefined objects `Logic0`, `Logic1`, `Logic2` and `Logic3`.
+Class for interfacing with the built-in logic block. use the predefined objects `Logic0`, `Logic1`, `Logic2` and `Logic3`.  
+`Logic2` and `Logic3` are only available on the ATmega parts, `Logic4` and `Logic5` only on AVR-DA.
 Each object contains register pointers for interfacing with the right registers. Some of these variables are available to the user.
 
 
@@ -26,26 +28,79 @@ Logic0.enable = true; // Enable logic block 0
 ##### Default state
 `Logic0.enable` defaults to `false` if not specified in the user program.
 
-
 ### input0..input2
-Variable for setting what mode input 0..2 on a logic block should have.  
-Accepted values:
+Variable for setting what mode input 0..2 on a logic block should have.
+
+Notes for all parts:
+* The datasheets give different names for the event channels for ATtiny and ATmega parts, both are supported by library: event 0 and event a are the same, as are event 1 and event b. These can be generated using the event system, see the relevant datasheet and examples for more information.
+* Timer WO channels correspond to the specified timer's PWM Output channels. They are true when the specified timer's value is higher than the compare register for that output channel.
+* Type-B timers can only generate an output under circumstances where they could drive an output pin if CCMPEN in TCBn.CTRLA is set.
+
+Accepted values for DA-series and megaAVR 0-series parts:
 ``` c++
-in::masked;       // Pin not in use
-in::unused;       // Pin not in use
-in::disable;      // Pin not in use
-in::feedback;     // Connect output of the logic block to this input
-in::link;         // Connect output of logic block n+1 to this input
-in::event_a;      // Connect input to event A
-in::event_b;      // Connect input to event B
-in::input;        // Connect input to GPIO
-in::ac;           // Connect input to the output of the internal analog comparator
-in::input_pullup; // Connect input to GPIO and enable the internal pullup resistor
-in::uart;         // Connect input to UART TX. Input 0 connects to UART0 TX, input 1 to UART1 TX, and input 2 to UART2 TX
-in::spi;          // Connect input to SPI. Input 0 and 1 connects to MOSI, and input 2 connects to SCK
-in::tca0;         // Connect input to TCA0. Input 0 connects to WO0, input 1 to WO1 and input2 to WO2
-in::tcb;          // Connect input to TCB. Input 0 connects to TCB0 W0, input 1 to TCB1 WO, and input 2 to TCB2 WO
+in::masked;           // Pin not in use
+in::unused;           // Pin not in use
+in::disable;          // Pin not in use
+in::feedback;         // Connect output of the logic block to this input
+in::link;             // Connect output of logic block n+1 to this input
+in::event_0;          // Connect input to event a
+in::event_a;          // Connect input to event a
+in::event_1;          // Connect input to event b
+in::event_b;          // Connect input to event b
+in::pin;              // Connect input to CCL IN0, IN1, IN2 for input 0, 1, 2, do not change pinMode
+in::input_pullup;     // Connect input to CCL IN0, IN1, IN2 for input 0, 1, 2, set input, pullup on
+in::input;            // Connect input to CCL IN0, IN1, IN2 for input 0, 1, 2, set input, pullup off
+in::input_no_pullup;  // Connect input to CCL IN0, IN1, IN2 for input 0, 1, 2, set input, pullup off
+in::ac;               // Connect input to the output of the internal analog comparator (input 0,1,2 from AC0,1,2)
+in::zcd;              // Connect input to the output of the zero crossing detector (input 0,1,2 from ZCD0,1,2) - DA-series only
+in::uart;             // Connect input to UART TX. Input 0 connects to UART0 TX, input 1 to UART1 TX, and input 2 to UART2 TX
+in::spi;              // Connect input to SPI. Input 0 and 1 connects to MOSI, and input 2 connects to SCK
+in::tca0;             // Connect input to TCA0. Input 0 connects to WO0, input 1 to WO1 and input2 to WO2
+in::tca1;             // Connect input to TCA1. Input 0 connects to WO0, input 1 to WO1 and input2 to WO2 - DA-series only
+in::tcb;              // Connect input to TCB. Input 0 connects to TCB0 W0, input 1 to TCB1 WO, and input 2 to TCB2 WO.
+in::tcd;              // Connect input to TCD0. Input 0 connects to WOA, input 1 to WOB and input2 to WOC - DA-series only
 ```
+
+Notes specific to ATmega parts
+* The 48-pin and 64-pin DA-series parts have 6 LUTs. No pins for CCL5 are available in the 48-pin package.
+* On 28-pin versions of the ATmega 4808, 3208, 1608, and 808, IN1 and IN2 inputs for logic3 are not available. If all input pins for all logic blocks are needed, the event system workaround shown for the ATtiny parts in the examples can be used, though note that register names and values are different. Same principle applies to the DA-series devices with fewer pins.
+* According to the datasheet for SPI as input source, inputs 0 and 1 connect to MOSI. Thus, on these parts, there is no input to the logic blocks for MISO. Note also that the order is different from the ATtiny parts.
+* If input on the highest-number Logic block is set to link, it will use the output of Logic0 (unless it's an early production DA-series - see the errata)
+* If you need to link input to logic block other than the n+1 block, you can use the event system for that.
+
+Accepted values for tinyAVR 0/1-series parts:
+
+``` c++
+in::masked;           // Pin not in use
+in::unused;           // Pin not in use
+in::disable;          // Pin not in use
+in::feedback;         // Connect output of the logic block to this input
+in::link;             // Connect output of the other logic block to this input
+in::event_0;          // Connect input to event 0
+in::event_a;          // Connect input to event 0
+in::event_1;          // Connect input to event 1
+in::event_b;          // Connect input to event 1
+in::pin;              // Connect input to CCL IN0, IN1, IN2 for input 0, 1, 2, do not change pinMode
+in::input_pullup;     // Connect input to CCL IN0, IN1, IN2 for input 0, 1, 2, set input, pullup on
+in::input;            // Connect input to CCL IN0, IN1, IN2 for input 0, 1, 2, set input, pullup off
+in::input_no_pullup;  // Connect input to CCL IN0, IN1, IN2 for input 0, 1, 2, set input, pullup off
+in::ac0;              // Connect input to AC0 OUT
+in::tcb0;             // Connect input to TCB0 WO
+in::tca;              // Connect input to TCA0 WO0~2 for input 0~2
+in::tca0;             // Connect input to TCA0 WO0~2 for input 0~2
+in::tcd;              // Connect input to TCD WOAn, WOB, WOA for input 0, 1, 2
+in::usart;            // Connect input to U(S)ART0 XCK and TXD for input 0, 1
+in::spi;              // Connect input to SPI0 SCK, MOSI, MISO for input 0, 1, 2
+in::ac1;              // Connect input to AC1 OUT (input 0, 1 only)
+in::tcb1;             // Connect input to TCB1 WO (input 0, 1 only)
+in::ac2;              // Connect input to AC2 OUT (input 0, 1 only)
+```
+
+Notes specific to ATtiny:
+* It is not clear what TCD0 WOAn is. I suspect it is true when TCD0 count is greater than TCD0.COMPSET0 and TCD0.CMPCLR0, ie, when WOA turns on and off, which can be individually controlled; the timer continues counting until reaching TCD0.CMPCLR1 when WOB turns off (TCD0 is a very strange timer).
+* Not all inputs are available on all parts - only input sources corresponding to peripherals on that device are available. Not all options are avalable for input 2, as noted above.
+* On CCL1 (logic1), IN0 I/O input is available only on the 20 and-24 pin parts, and IN1 and IN2 only on 24-pin parts. The event inputs can be used with pin events to take input from a diferent pins for up to two inputs. This is demonstrated in the five input example.
+* CCL0's IN0 pin is on PA0, which is nominally the UPDI pin. It can only be used as input when that pin is set as GPIO. This limits the usefulness of CCL0 on the ATtiny parts; configuring UPDI as GPIO prevents further programming via UPDI except via HV programming. Configuring this option is only supported on megaTinyCore with Optiboot bootloader (from 1.1.6 on), and prevents further modification of the fuses or bootloader without HV programming, however, as noted above, input0 can be used via the event inputs to take input from another pin. This is demonstrated in the three input examples.
 
 ##### Usage
 ``` c++
@@ -97,8 +152,8 @@ Variable to connecting a filter or synchronizer to the logic block output. Usefu
 Accepted values:
 ```c++
 filter::disable;      // No filter used
-filter::synchronizer; // Connect synchronizer to output
-filter::filter;       // Connect filter to output
+filter::synchronizer; // Connect synchronizer to output; delays output by 2 clock cycles.
+filter::filter;       // Connect filter to output; delays output by 4 clock cycles, only passes output that is stable for >2 clock cycles.
 ```
 
 ##### Usage
@@ -110,8 +165,45 @@ Logic0.filter = filter::filter; // Enable filter on output of block 0
 `LogicN.filter` defaults to `filter::disable` if not specified in the user program.
 
 
+### clocksource
+Variable set the clock source for the LUT. The inputs are checked and output updated on the rising edge of this clock. Except on the Dx-series parts, only clk_per (peripheral clock, ie, the system clock) and in2 are available. If you don't know what this means, you want clk_per. If sequential logic is used, this is also used to clock that.
+Accepted values:
+```c++
+clocksource::clk_per;      // Clock from the peripheral clock (ie, system clock)
+clocksource::in2;          // Clock from the selected input2; it is treated as a 0 in the truth table.
+clocksource::oschf;        // Clock from the **unprescaled** internal HF oscillator. Same as clk_per if system clock not prescaled.
+clocksource::osc32k;       // Clock from the internal 32.768 kHz oscillator
+clocksource::osc1l;        // Clock from the internal 32.768 kHz oscillator prescaled by 32
+```
+
+#### Usage
+```c++
+Logic0.clocksource = clocksource::clk_per;
+```
+
+##### Default state
+`LogicN.clocksource` defaults to `clocksource::clk_per` if not specified in the user program.
+
+
+### edgedetect
+Variable for controlling use of the edge detector. The edge detector can be used to generate a pulse when detecting a rising edge on its input. To detect a falling edge, the TRUTH table should be programmed to provide inverted output. In order to avoid unpredictable behavior, a valid filter option must be enabled. Note that this is likely only of use when the output is being used for sequential logic or as the input to another logic block; it looks particularly useful on the odd LUT input to a J-K flip-flop sequential logic unit.
+
+```c++
+edgedetect::disable;      // No edge detection used
+edgedetect::enable;       // Edge detection used
+```
+
+#### Usage
+```c++
+Logic0.edgedetect = edgedetect::enable;
+```
+
+##### Default state
+`LogicN.edgedetect` defaults to `edgedetect::disable` if not specified in the user program.
+
+
 ### sequencer
-Variable for connecting a sequencer to the logic block output.  
+Variable for connecting a sequencer to the logic block output. There is 1 sequencer per 2 CCLs; this option is ignored for the odd-numbered logic blocks.
 Accepted values:
 ```c++
 sequencer::disable;      // No sequencer connected
@@ -178,7 +270,7 @@ Logic::stop(); // Stop CCL
 ## attachInterrupt()
 Method for enabling interrupts for a specific block.  
 Valid arguments for the third parameters are `RISING`, `FALLING` and `CHANGE`.
-This method ins't available on tinyAVR series.
+This method ins't available on tinyAVR series, as these parts cannot generate an interrupt from the CCL blocks.
 
 ##### Usage
 ```c++
--- megaavr/libraries/Logic/src/Logic.cpp
+++ megaavr/libraries/Logic/src/Logic.cpp
@@ -1,47 +1,52 @@
 #include "Logic.h"
 
-#if defined(CCL_CCL_vect)
 // Array for storing ISR function pointers
+#if defined(CCL_CCL_vect)
+#if defined(TRUTH5)
+static volatile voidFuncPtr intFuncCCL[6];
+#else
 static volatile voidFuncPtr intFuncCCL[4];
+#endif
 #endif
 
 struct Logic::CCLBlock
 {
-    const uint8_t number;
-    const uint8_t input0_bm;
-    const uint8_t input1_bm;
-    const uint8_t input2_bm;
-    const uint8_t output_bm;
-    const uint8_t output_alt_bm;
-    PORT_t& PORT_IN;
-    PORT_t& PORT_OUT;
-    PORT_t& PORT_ALT_OUT;
-    volatile register8_t& SEQCTRL;
-    volatile register8_t& LUTCTRLA;
-    volatile register8_t& LUTCTRLB;
-    volatile register8_t& LUTCTRLC;
-    volatile register8_t& TRUTH;
+  const uint8_t number;
+  const uint8_t input0_bm;
+  const uint8_t input1_bm;
+  const uint8_t input2_bm;
+  const uint8_t output_bm;
+  const uint8_t output_alt_bm;
+  PORT_t& PORT_IN;
+  PORT_t& PORT_OUT;
+  PORT_t& PORT_ALT_OUT;
+  volatile register8_t& SEQCTRL;
+  volatile register8_t& LUTCTRLA;
+  volatile register8_t& LUTCTRLB;
+  volatile register8_t& LUTCTRLC;
+  volatile register8_t& TRUTH;
 };
 
-static const struct Logic::CCLBlock blocks[] = {
+static const struct Logic::CCLBlock blocks[] =
+{
 #if defined(__AVR_ATtiny202__) || defined(__AVR_ATtiny402__) ||  \
     defined(__AVR_ATtiny204__) || defined(__AVR_ATtiny404__) ||  \
     defined(__AVR_ATtiny804__) || defined(__AVR_ATtiny1604__) || \
     defined(__AVR_ATtiny212__) || defined(__AVR_ATtiny412__) ||  \
     defined(__AVR_ATtiny214__) || defined(__AVR_ATtiny414__) ||  \
     defined(__AVR_ATtiny814__) || defined(__AVR_ATtiny1614__)
-    {
-      0,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN6_bm, 0,
-      PORTA, PORTA, PORTA,
-      CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
-    },
-    {
-      1,
-      0, 0, 0, PIN7_bm, 0,
-      PORTA, PORTA, PORTA,
-      CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
-    },
+  {
+    0,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN6_bm, 0,
+    PORTA, PORTA, PORTA,
+    CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
+  },
+  {
+    1,
+    0, 0, 0, PIN7_bm, 0,
+    PORTA, PORTA, PORTA,
+    CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
+  },
 #endif
 #if defined(__AVR_ATtiny406__) || defined(__AVR_ATtiny806__) || \
     defined(__AVR_ATtiny1606__) ||                              \
@@ -49,94 +54,116 @@ static const struct Logic::CCLBlock blocks[] = {
     defined(__AVR_ATtiny1616__) || defined(__AVR_ATtiny3216__)
 #define PORTMUX_CCL PORTMUX.CTRLA
 #define PORTMUX_ALTOUT_bm (1 << (block.number + 4))
-    {
-      0,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN6_bm, PIN4_bm,
-      PORTA, PORTA, PORTB,
-      CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
-    },
-    {
-      1,
-      PIN3_bm, 0, 0, PIN7_bm, PIN1_bm,
-      PORTC, PORTA, PORTC,
-      CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
-    },
+  {
+    0,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN6_bm, PIN4_bm,
+    PORTA, PORTA, PORTB,
+    CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
+  },
+  {
+    1,
+    PIN3_bm, 0, 0, PIN7_bm, PIN1_bm,
+    PORTC, PORTA, PORTC,
+    CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
+  },
 #endif
 #if defined(__AVR_ATtiny807__) || defined(__AVR_ATtiny1607__) || \
     defined(__AVR_ATtiny417__) || defined(__AVR_ATtiny817__) ||  \
     defined(__AVR_ATtiny1617__) || defined(__AVR_ATtiny3217__)
 #define PORTMUX_CCL PORTMUX.CTRLA
 #define PORTMUX_ALTOUT_bm (1 << (block.number + 4))
-    {
-      0,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN6_bm, PIN4_bm,
-      PORTA, PORTA, PORTB,
-      CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
-    },
-    {
-      1,
-      PIN3_bm, PIN4_bm, PIN5_bm, PIN7_bm, PIN1_bm,
-      PORTC, PORTA, PORTC,
-      CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
-    },
+  {
+    0,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN6_bm, PIN4_bm,
+    PORTA, PORTA, PORTB,
+    CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
+  },
+  {
+    1,
+    PIN3_bm, PIN4_bm, PIN5_bm, PIN7_bm, PIN1_bm,
+    PORTC, PORTA, PORTC,
+    CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
+  },
 #endif
-#if defined(__AVR_ATmega808__) || defined(__AVR_ATmega1608__) || \
-    defined(__AVR_ATmega3208__) || defined(__AVR_ATmega4808__)
+#if defined(__AVR_ATmega808__) || defined(__AVR_ATmega1608__) ||  \
+    defined(__AVR_ATmega3208__) || defined(__AVR_ATmega4808__)||  \
+    defined(__AVR_AVR128DA32__)|| defined(__AVR_AVR128DA28__) ||  \
+    defined(__AVR_AVR64DA32__) || defined(__AVR_AVR64DA28__) ||   \
+    defined(__AVR_AVR32DA32__) || defined(__AVR_AVR32DA28__)
 #define PORTMUX_CCL PORTMUX.CCLROUTEA
 #define PORTMUX_ALTOUT_bm (1 << block.number)
-    {
-      0,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
-      PORTA, PORTA, PORTA,
-      CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
-    },
-    {
-      1,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, 0,
-      PORTC, PORTC, PORTC,
-      CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
-    },
-    {
-      2,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
-      PORTD, PORTD, PORTD,
-      CCL.SEQCTRL1, CCL.LUT2CTRLA, CCL.LUT2CTRLB, CCL.LUT2CTRLC, CCL.TRUTH2,
-    },
-    { // Note: 28-Pin version doesn't have input2 and output_alt.
-      3,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
-      PORTF, PORTF, PORTF,
-      CCL.SEQCTRL1, CCL.LUT3CTRLA, CCL.LUT3CTRLB, CCL.LUT3CTRLC, CCL.TRUTH3,
-    },
+  {
+    0,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTA, PORTA, PORTA,
+    CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
+  },
+  {
+    1,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, 0,
+    PORTC, PORTC, PORTC,
+    CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
+  },
+  {
+    2,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTD, PORTD, PORTD,
+    CCL.SEQCTRL1, CCL.LUT2CTRLA, CCL.LUT2CTRLB, CCL.LUT2CTRLC, CCL.TRUTH2,
+  },
+  { // Note: 28-Pin version doesn't have input2 and output_alt
+    3,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTF, PORTF, PORTF,
+    CCL.SEQCTRL1, CCL.LUT3CTRLA, CCL.LUT3CTRLB, CCL.LUT3CTRLC, CCL.TRUTH3,
+  },
 #endif
-#if defined(__AVR_ATmega809__) || defined(__AVR_ATmega1609__) || \
-    defined(__AVR_ATmega3209__) || defined(__AVR_ATmega4809__)
+#if defined(__AVR_ATmega809__) || defined(__AVR_ATmega1609__) ||  \
+    defined(__AVR_ATmega3209__) || defined(__AVR_ATmega4809__) || \
+    defined(__AVR_AVR128DA64__) || defined(__AVR_AVR128DA48__) || \
+    defined(__AVR_AVR64DA64__) || defined(__AVR_AVR64DA48__) ||   \
+    defined(__AVR_AVR32DA64__) || defined(__AVR_AVR32DA48__)
 #define PORTMUX_CCL PORTMUX.CCLROUTEA
 #define PORTMUX_ALTOUT_bm (1 << block.number)
-    {
-      0,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
-      PORTA, PORTA, PORTA,
-      CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
-    },
-    { // Note: 40-Pin version doesn't have output_alt.
-      1,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
-      PORTC, PORTC, PORTC,
-      CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
-    },
-    {
-      2,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
-      PORTD, PORTD, PORTD,
-      CCL.SEQCTRL1, CCL.LUT2CTRLA, CCL.LUT2CTRLB, CCL.LUT2CTRLC, CCL.TRUTH2,
-    },
-    {
-      3,
-      PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
-      PORTF, PORTF, PORTF,
-      CCL.SEQCTRL1, CCL.LUT3CTRLA, CCL.LUT3CTRLB, CCL.LUT3CTRLC, CCL.TRUTH3,
-    },
+  {
+    0,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTA, PORTA, PORTA,
+    CCL.SEQCTRL0, CCL.LUT0CTRLA, CCL.LUT0CTRLB, CCL.LUT0CTRLC, CCL.TRUTH0,
+  },
+  { // Note: 40-Pin version doesn't have output_alt
+    1,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTC, PORTC, PORTC,
+    CCL.SEQCTRL0, CCL.LUT1CTRLA, CCL.LUT1CTRLB, CCL.LUT1CTRLC, CCL.TRUTH1,
+  },
+  {
+    2,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTD, PORTD, PORTD,
+    CCL.SEQCTRL1, CCL.LUT2CTRLA, CCL.LUT2CTRLB, CCL.LUT2CTRLC, CCL.TRUTH2,
+  },
+  {
+    3,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTF, PORTF, PORTF,
+    CCL.SEQCTRL1, CCL.LUT3CTRLA, CCL.LUT3CTRLB, CCL.LUT3CTRLC, CCL.TRUTH3,
+  },
+#endif
+#if defined(__AVR_AVR128DA64__) || defined(__AVR_AVR128DA48__) || \
+    defined(__AVR_AVR64DA64__) || defined(__AVR_AVR64DA48__) ||   \
+    defined(__AVR_AVR32DA64__) || defined(__AVR_AVR32DA48__)
+  {
+    4,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTB, PORTB, PORTB,
+    CCL.SEQCTRL2, CCL.LUT4CTRLA, CCL.LUT4CTRLB, CCL.LUT4CTRLC, CCL.TRUTH4,
+  },
+  {
+    5,
+    PIN0_bm, PIN1_bm, PIN2_bm, PIN3_bm, PIN6_bm,
+    PORTG, PORTG, PORTG,
+    CCL.SEQCTRL2, CCL.LUT5CTRLA, CCL.LUT5CTRLB, CCL.LUT5CTRLC, CCL.TRUTH5,
+  },
 #endif
 };
 
@@ -152,6 +179,12 @@ Logic Logic2(2);
 #if defined(CCL_TRUTH3)
 Logic Logic3(3);
 #endif
+#if defined(CCL_TRUTH4)
+Logic Logic4(4);
+#endif
+#if defined(CCL_TRUTH5)
+Logic Logic5(5);
+#endif
 
 Logic::Logic(const uint8_t block_number)
     : enable(false),
@@ -161,8 +194,10 @@ Logic::Logic(const uint8_t block_number)
       output(out::disable),
       output_swap(out::no_swap),
       filter(filter::disable),
+      edgedetect(edgedetect::disable),
       truth(0x00),
       sequencer(sequencer::disable),
+      clocksource(clocksource::clk_per),
       block(blocks[block_number])
 {
 }
@@ -192,38 +227,39 @@ static volatile register8_t& PINCTRL(PORT_t& port, const uint8_t pin_bm)
   return port.PIN7CTRL;
 }
 
-
 void Logic::initInput(in::input_t& input, PORT_t& port, const uint8_t pin_bm)
 {
-  if(input == in::input && pin_bm)
+  if((input & 0x30) && pin_bm) // Input pin is either set to input or input with pullup
   {
     port.DIRCLR = pin_bm;
-    PINCTRL(port, pin_bm) &= ~PORT_PULLUPEN_bm;
-  }
-  else if (input == in::input_pullup && pin_bm)
-  {
-    port.DIRCLR = pin_bm;
-    PINCTRL(port, pin_bm) |= PORT_PULLUPEN_bm;
+
+    if(input == in::input)
+      PINCTRL(port, pin_bm) |= PORT_PULLUPEN_bm;
+    else
+      PINCTRL(port, pin_bm) &= ~PORT_PULLUPEN_bm;
+
     input = in::input;
   }
 }
-
 
 void Logic::init()
 {
   initInput(input0, block.PORT_IN, block.input0_bm);
   initInput(input1, block.PORT_IN, block.input1_bm);
   initInput(input2, block.PORT_IN, block.input2_bm);
-  
+
   // Set inputs modes
   block.LUTCTRLB = (input1 << CCL_INSEL1_gp) | (input0 << CCL_INSEL0_gp);
   block.LUTCTRLC = (input2 << CCL_INSEL2_gp);
 
   // Set truth table
   block.TRUTH = truth;
-  
+
   // Set sequencer
-  block.SEQCTRL = sequencer;
+  if (!(block.number & 0x01))
+  {
+    block.SEQCTRL = sequencer;
+  }
 
   // Set output pin state and output pin swap
   if(output == out::enable)
@@ -243,19 +279,20 @@ void Logic::init()
       block.PORT_ALT_OUT.DIRSET = block.output_bm;
     }
   }
-  
+
   // Set logic output state and output filter
   block.LUTCTRLA = (output ? CCL_OUTEN_bm : 0)
-      | (filter << CCL_FILTSEL_gp)
-      | (enable ? CCL_ENABLE_bm : 0);
+    | (edgedetect ? CCL_EDGEDET_EN_gc : 0 )
+    | (filter << CCL_FILTSEL_gp)
+    | (clocksource << CCL_CLKSRC_gp)
+    | (enable ? CCL_ENABLE_bm : 0);
 }
-
 
 #if defined(CCL_CCL_vect)
 void Logic::attachInterrupt(void (*userFunc)(void), uint8_t mode)
 {
   CCL_INTMODE0_t intmode;
-  switch (mode) 
+  switch (mode)
   {
     // Set RISING, FALLING or CHANGE interrupt trigger for a block output
     case RISING:
@@ -271,27 +308,43 @@ void Logic::attachInterrupt(void (*userFunc)(void), uint8_t mode)
       // Only RISING, FALLING and CHANGE is supported
       return;
   }
-  const int intmode_bp = block.number * 2;
-  CCL.INTCTRL0 = (CCL.INTCTRL0 & ~(CCL_INTMODE0_gm << intmode_bp))
-      | (intmode << intmode_bp);
-  
+#if defined(CCL_TRUTH4)
+  if (block.number > 3)
+  {
+    const int16_t intmode_bp = (block.number & 0x03) * 2;
+    CCL.INTCTRL1 = (CCL.INTCTRL1 & ~(CCL_INTMODE0_gm << intmode_bp)) | (intmode << intmode_bp);
+  }
+  else
+  {
+    const int16_t intmode_bp = (block.number & 0x03) * 2;
+    CCL.INTCTRL0 = (CCL.INTCTRL0 & ~(CCL_INTMODE0_gm << intmode_bp)) | (intmode << intmode_bp);
+  }
+#else
+  const int16_t intmode_bp = block.number * 2;
+  CCL.INTCTRL0 = (CCL.INTCTRL0 & ~(CCL_INTMODE0_gm << intmode_bp)) | (intmode << intmode_bp);
+#endif
   // Store function pointer
   intFuncCCL[block.number] = userFunc;
 }
 
-
 void Logic::detachInterrupt()
 {
   // Disable interrupt for a given block output
+#if defined(CCL_TRUTH4)
+  if(block_number > 4)
+    CCL.INTCTRL1 &= ~(CCL_INTMODE1_gm << ((block.number & 3) * 2));
+  else
+    CCL.INTCTRL0 &= ~(CCL_INTMODE0_gm << (block.number * 2));
+#else
   CCL.INTCTRL0 &= ~(CCL_INTMODE0_gm << (block.number * 2));
+#endif
 }
-
 
 // CCL interrupt service routine
 // Use attachIntterupt to activate this.
 ISR(CCL_CCL_vect)
 {
-   // Cleck for block 0 interrupt
+  // Check for block 0 interrupt
   if(CCL.INTFLAGS & CCL_INT0_bm)
   {
     // Run user function
@@ -299,17 +352,23 @@ ISR(CCL_CCL_vect)
     // Clear flag
     CCL.INTFLAGS |= CCL_INT0_bm;
   }
+  // Check for block 1 interrupt
   if(CCL.INTFLAGS & CCL_INT1_bm)
   {
+    // Run user function
     intFuncCCL[CCL_INT1_bp]();
+    // Clear flag
     CCL.INTFLAGS |= CCL_INT1_bm;
   }
+  // Check for block 2 interrupt
   if(CCL.INTFLAGS & CCL_INT2_bm)
   {
+    // Run user function
     intFuncCCL[CCL_INT2_bp]();
+    // Clear flag
     CCL.INTFLAGS |= CCL_INT2_bm;
   }
-  // Cleck for block 3 interrupt
+  // Check for block 3 interrupt
   if(CCL.INTFLAGS & CCL_INT3_bm)
   {
     // Run user function
@@ -317,5 +376,26 @@ ISR(CCL_CCL_vect)
     // Clear flag
     CCL.INTFLAGS |= CCL_INT3_bm;
   }
-}
+#if defined(TRUTH4)
+  // Check for block 4 interrupt
+  if(CCL.INTFLAGS & CCL_INT4_bm)
+  {
+    // Run user function
+    intFuncCCL[CCL_INT4_bp]();
+    // Clear flag
+    CCL.INTFLAGS |= CCL_INT4_bm;
+  }
+#endif
+#if defined(TRUTH5)
+  // Check for block 5 interrupt
+  if(CCL.INTFLAGS & CCL_INT5_bm)
+  {
+    // Run user function
+    intFuncCCL[CCL_INT5_bp]();
+    // Clear flag
+    CCL.INTFLAGS |= CCL_INT5_bm;
+  }
 #endif // CCL_CCL_vect
+}
+
+#endif
