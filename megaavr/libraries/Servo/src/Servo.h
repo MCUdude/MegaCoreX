c2321a73e51144e0484bab78f351293d072218c7
MCUdude <hansibull@gmail.com>
2020-11-16 11:28:50 +0100

    Update Servo library It now behaves like the official Servo library. Closes #95

--- megaavr/libraries/Servo/src/Servo.h
+++ megaavr/libraries/Servo/src/Servo.h
@@ -47,16 +47,15 @@
 
 #ifndef Servo_h
 #define Servo_h
-
 #include <inttypes.h>
 
 /*
- * Defines for 16 bit timers used with  Servo library
- *
- * If _useTimerX is defined then TimerX is a 16 bit timer on the current board
- * timer16_Sequence_t enumerates the sequence that the timers should be allocated
- * _Nbr_16timers indicates how many 16 bit timers are available.
- */
+   Defines for 16 bit timers used with  Servo library
+
+   If _useTimerX is defined then TimerX is a 16 bit timer on the current board
+   timer16_Sequence_t enumerates the sequence that the timers should be allocated
+   _Nbr_16timers indicates how many 16 bit timers are available.
+*/
 
 // Architecture specific include
 #if defined(ARDUINO_ARCH_MEGAAVR)
@@ -65,47 +64,51 @@
     #warning "Running at 1MHz results in unstable servo signal."
   #endif
 #else
-  #error "This library only supports boards with a megaAVR-0 processor."
+  #error "This is an architecture specific library for ARDUINO_ARCH_MEGAAVR, but this device is not of that architecture"
 #endif
 
-#define Servo_VERSION           2     // software version of this library
+#define Servo_VERSION        2         // software version of this library
 
-#define MIN_PULSE_WIDTH       544     // the shortest pulse sent to a servo
-#define MAX_PULSE_WIDTH      2400     // the longest pulse sent to a servo
-#define DEFAULT_PULSE_WIDTH  1500     // default pulse width when servo is attached
-#define REFRESH_INTERVAL    20000     // minumim time to refresh servos in microseconds
+#define MIN_PULSE_WIDTH      544       // the shortest pulse sent to a servo
+#define MAX_PULSE_WIDTH      2400      // the longest pulse sent to a servo
+#define DEFAULT_PULSE_WIDTH  1500      // default pulse width when servo is attached
+#define REFRESH_INTERVAL     20000UL   // minumim time to refresh servos in microseconds
 
-#define SERVOS_PER_TIMER       12     // the maximum number of servos controlled by one timer
+#define SERVOS_PER_TIMER     12        // the maximum number of servos controlled by one timer
 #define MAX_SERVOS   (_Nbr_16timers  * SERVOS_PER_TIMER)
 
-#define INVALID_SERVO         255     // flag indicating an invalid servo index
+#define INVALID_SERVO        255       // flag indicating an invalid servo index
 
 typedef struct  {
-  uint8_t nbr        :6 ;             // a pin number from 0 to 63
-  uint8_t isActive   :1 ;             // true if this channel is enabled, pin not pulsed if false
-} ServoPin_t   ;
+  //uint8_t nbr;                       // a pin number from 0 to 63
+  // the original implementation with bitslices and just isActive and nbr in a single byte saved 2 bytes of ram per servo, as a cost of a very small amount of flash.
+  // optimization it supports (losing digitalWrite() in the ISR) has surprisingly little impact - though I do think it has a more significant impact on time spent
+  // in the ISR, which you don't see most of the time...
+  uint8_t isActive;                    // true if this channel is enabled, pin not pulsed if false
+  uint8_t port;
+  uint8_t bitmask;
+} ServoPin_t;
 
 typedef struct {
   ServoPin_t Pin;
-  volatile unsigned int ticks;
+  volatile uint16_t ticks;
 } servo_t;
 
-class Servo
-{
-public:
-  Servo();
-  uint8_t attach(uint8_t pin);       // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
-  uint8_t attach(uint8_t pin, int16_t min, int16_t max); // as above but also sets min and max values for writes.
-  void detach();
-  void write(int16_t value);         // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds
-  void writeMicroseconds(int16_t value); // Write pulse width in microseconds
-  int read();                        // returns current pulse width as an angle between 0 and 180 degrees
-  int readMicroseconds();            // returns current pulse width in microseconds for this servo (was read_us() in first release)
-  bool attached();                   // return true if this servo is attached, otherwise false
-private:
-   uint8_t servoIndex;               // index into the channel data for this servo
-   int8_t min;                       // minimum is this value times 4 added to MIN_PULSE_WIDTH
-   int8_t max;                       // maximum is this value times 4 added to MAX_PULSE_WIDTH
+class Servo {
+  public:
+    Servo();
+    uint8_t attach(uint8_t pin);                           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
+    uint8_t attach(uint8_t pin, int16_t min, int16_t max); // as above but also sets min and max values for writes.
+    void detach();
+    void write(uint16_t value);                            // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds
+    void writeMicroseconds(uint16_t value);                // Write pulse width in microseconds
+    int16_t read();                                        // returns current pulse width as an angle between 0 and 180 degrees
+    uint16_t readMicroseconds();                           // returns current pulse width in microseconds for this servo (was read_us() in first release)
+    bool attached();                                       // return true if this servo is attached, otherwise false
+  private:
+    uint8_t servoIndex;                                    // index into the channel data for this servo
+    int8_t min;                                            // minimum is this value times 4 added to MIN_PULSE_WIDTH
+    int8_t max;                                            // maximum is this value times 4 added to MAX_PULSE_WIDTH
 };
 
 #endif
