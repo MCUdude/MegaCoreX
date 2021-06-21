cd2a1fde7b868c623973f193b36cbb8dcd35247e
MCUdude <hansibull@gmail.com>
2021-06-21 18:34:51 +0200

    Update to C++17

--- megaavr/cores/coreX-corefiles/new.cpp
+++ megaavr/cores/coreX-corefiles/new.cpp
@@ -37,3 +37,18 @@ void operator delete[](void *ptr)
 {
   free(ptr);
 }
+
+// C++14 introduces additional delete operators
+#if __cplusplus >= 201402L
+
+void operator delete(void * ptr, size_t)
+{
+  ::operator delete(ptr);
+}
+
+void operator delete[](void * ptr, size_t)
+{
+  ::operator delete(ptr);
+}
+
+#endif // End language is C++14 or greater
